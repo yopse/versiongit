666e72779af4c1e6e6b263a3b1d7f12a9df47413
yopse <82827725+yopse@users.noreply.github.com>
2022-09-03 16:24:14 +0530

    all fusion chart are added succesfully

--- src/components/Charts/Bar3D.js
+++ src/components/Charts/Bar3D.js
@@ -1,38 +1,23 @@
-// STEP 1 - Include Dependencies
-// Include react
 import React from "react";
 
-// Include the react-fusioncharts component
 import ReactFC from "react-fusioncharts";
 
-// Include the fusioncharts library
 import FusionCharts from "fusioncharts";
 
-// Include the chart type
 import Chart from "fusioncharts/fusioncharts.charts";
 
-// Include the theme as fusion
 import FusionTheme from "fusioncharts/themes/fusioncharts.theme.fusion";
 
-// Adding the chart and theme as dependency to the core fusioncharts
 ReactFC.fcRoot(FusionCharts, Chart, FusionTheme);
-
-// STEP 2 - Chart Data
-
-// STEP 3 - Creating the JSON object to store the chart configurations
-
-// STEP 4 - Creating the DOM element to pass the react-fusioncharts componerat
 
 const Column3D = ({ data }) => {
   const chartConfigs = {
-    type: "bar3d", // The chart type
-    width: "100%", // Width of the chart
-    height: "400", // Height of the chart
-    dataFormat: "json", // Data type
+    type: "bar3d",
+    width: "100%",
+    height: "400",
+    dataFormat: "json",
     dataSource: {
-      // Chart Configuration
       chart: {
-        //Set the chart caption
         caption: "Most Forked",
         decimals: 0,
 
@@ -41,7 +26,7 @@ const Column3D = ({ data }) => {
         yAxisNameFontSize: "16px",
         xAxisNameFontSize: "16px",
       },
-      // Chart Data
+
       data,
     },
   };
